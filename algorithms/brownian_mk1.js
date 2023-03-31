2da6b3fdbd170e34f087d9444374eff2a99647e9
vm15717 <vm15717@my.bristol.ac.uk>
2023-03-31 16:56:26 +0100

    updated brownian motion code to change background to black and also the stroke

--- algorithms/brownian_mk1.js
+++ algorithms/brownian_mk1.js
@@ -8,26 +8,28 @@ function setup() {
     for (i = 1; i <= array_len; i+= 1){
         x[i] = x[i-1] + random(-5,5)
         y[i] = y[i-1] + random(-5,5)
-        //x[i] = constrain(x[i], 0, width);
-        //y[i] = constrain(y[i], 0, height); 
+        x[i] = constrain(x[i], 0, width);
+        y[i] = constrain(y[i], 0, height); 
     } 
     frameRate(30);
     //noLoop();
 }
   
 function draw() {
-    background(250);
+    background(0);
     let space = 10;
     for (i = 1; i <array_len; i+= 1){
         x[i - 1] = x[i]
         y[i - 1] = y[i]
-        //x[i] = constrain(x[i], 0, width);
-        //y[i] = constrain(y[i], 0, height); 
+        x[i] = constrain(x[i], 0, width);
+        y[i] = constrain(y[i], 0, height); 
     }  
     x[array_len - 1] = x[array_len - 1] + random(-5, 5)
     y[array_len - 1] = y[array_len - 1] + random(-5, 5)
-    stroke(128, 64, 0);    
+    //stroke(128, 64, 0);    
     for (j = 1; j <array_len; j += 1){
+        let val = j / array_len * 204.0 + 51;
+        stroke(val);
         line(x[j - 1],y[j - 1],x[j], y[j]);
     }
 }
